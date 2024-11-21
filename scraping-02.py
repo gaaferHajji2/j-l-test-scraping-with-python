1b8cd27b36c22fd8508edb80d34f280ef80e2414
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-21 22:28:55 +0200

    Adding Scraping-02 With bs4-module

---
+++ scraping-02.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup as bs;
+
+import requests;
+
+data = requests.get('http://pythonscraping.com/pages/page1.html');
+
+t1 = bs(data.content, 'html.parser');
+
+print(t1.h1);
