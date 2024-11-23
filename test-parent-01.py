e4a7de6abc59dbeb62f409166a4d2286c84784e3
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-23 15:26:19 +0300

    Test parent

---
+++ test-parent-01.py
@@ -0,0 +1,9 @@
+import requests;
+
+from bs4 import BeautifulSoup;
+
+data = requests.get('http://www.pythonscraping.com/pages/page3.html');
+
+bs = BeautifulSoup(data.content, 'lxml');
+
+print(bs.find('img',{'src':'../img/gifts/img1.jpg'}).parent.previous_sibling.get_text())
