7854a29020aa911cf59b802957b5dd818006c538
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-23 14:57:22 +0300

    Modifying script-04.py

--- scraping-04.py
+++ scraping-04.py
@@ -8,7 +8,13 @@ bs = BeautifulSoup(data.content, 'lxml');
 
 t1 = bs.find_all('span', attrs={'class': 'green'});
 
+t2 = bs.find_all(text='the prince')
+
+t3 = bs.find_all(id='title')
+
 print("The Green Span List Length is: ", len(t1));
+print("The Text Content 'The Prince' List Length is: ", len(t2));
+print("By Id And Class_ List Length is: ", len(t3));
 
 for name in t1:
     print(name.get_text());
