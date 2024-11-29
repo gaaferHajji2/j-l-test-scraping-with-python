4ad7902f08f70d194372fb25937c43a1911bd485
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-29 20:28:58 +0300

    Test Lambda Expression To Get Tags

---
+++ test-lambda-with-attrs-01.py
@@ -0,0 +1,15 @@
+import requests;
+
+from bs4 import BeautifulSoup;
+
+data = requests.get('http://www.pythonscraping.com/pages/page3.html');
+
+bs = BeautifulSoup(data.content, 'lxml');
+
+tags = bs.find_all(lambda tag: len(tag.attrs) >= 2 );
+
+if tags is not None:
+    for tag in tags:
+        print("The Tag Is: ", tag);
+else:
+    print("No Tags For Condition Found");
