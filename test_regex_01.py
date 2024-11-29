250bfbe33a2e2b984d704e7a6980ae001b2c5e7d
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-29 20:20:40 +0300

    Testing Regex To Find Images

---
+++ test_regex_01.py
@@ -0,0 +1,17 @@
+import requests;
+
+from bs4 import BeautifulSoup;
+
+import re;
+
+data = requests.get("http://www.pythonscraping.com/pages/page3.html");
+
+bs = BeautifulSoup(data.content, 'lxml');
+
+images = bs.find_all('img', { 'src': re.compile('..\/img\/gifts/img.*.jpg') } );
+
+if images is not None:
+    for image in images:
+        print("The Image src is: ", image['src']);
+else:
+    print("No Images Found");
