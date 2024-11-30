430d16a719f383b3d4e982c216f06e1970c6ca07
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-11-30 21:09:44 +0300

    Adding Exceptions Handling

--- test-attrs-href-with-function-01.py
+++ test-attrs-href-with-function-01.py
@@ -13,9 +13,16 @@ random.seed(datetime.datetime.now().__str__());
 def getLinks(article_url):
     data = requests.get(f'http://en.wikipedia.org{article_url}');
     bs = BeautifulSoup(data.content, 'lxml');
+    try:
 
-    return bs.find('div', { 'id': 'bodyContent' }) \
-    .find_all('a', { 'href': re.compile('^(/wiki/)((?!:).)*$') });
+        return bs.find('div', { 'id': 'bodyContent' }) \
+        .find_all('a', { 'href': re.compile('^(/wiki/)((?!:).)*$') });
+    except AttributeError as e:
+        print("Error Occurred With Data: ", e.__str__());
+        return [];
+    except Exception as ex:
+        print("General Error Happened: ", ex.__str__());
+        return [];
 
 links = getLinks('/wiki/Kevin_Bacon');
 
