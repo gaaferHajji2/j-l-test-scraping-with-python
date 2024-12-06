05366bd7852a892203ae7123570b8b2bc2f2f084
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-06 20:12:37 +0300

    Build Scraper Function With Crawler For Multiple WebSites

--- Scrape-Models/Crawler.py
+++ Scrape-Models/Crawler.py
@@ -7,30 +7,31 @@ from content import Content;
 from website import Website;
 
 class Crawler:
-    def getPage(self, url):
+    def get_page(self, url):
         try:
             data = requests.get(url);
         except Exception as e:
             print("Error In Get Page: ", e.__str__());
             return None;
+        # print("The Data For BS Is: ", data.content);
         return BeautifulSoup(data.content, 'lxml');
 
-    def getElements(self, bs, selector):
+    def get_elements(self, bs, selector):
         # select Returns Empty String If No Object Found
         items = bs.select(selector);
 
         if items is not None and len(items)> 0:
-            return '\n'.join([item.get_text().trim() for item in items]);
+            return '\n'.join([item.get_text().strip() for item in items]);
         else:
             print(f"No Items Found For selector {selector}");
 
-    def getContent(self, website: Website, path) -> Content:
+    def get_content(self, website: Website, path) -> Content:
         url = website.url + path;
-        bs = self.getPage(url);
+        bs = self.get_page(url);
 
         if bs is not None:
-            title = self.getElements(bs, website.titleTag);
-            body  = self.getElements(bs, website.bodyTag);
+            title = self.get_elements(bs, website.titleTag);
+            body  = self.get_elements(bs, website.bodyTag);
 
             return Content(url, title, body);
     
--- Scrape-Models/test_scraper_01.py
+++ Scrape-Models/test_scraper_01.py
@@ -1,8 +1,35 @@
 from website import Website;
 
+from crawler import Crawler;
+
 siteData = [
     ['O\'Reilly', 'https://www.oreilly.com', 'h1', 'div.title-description'],
-    ['Reuters', 'https://www.reuters.com', 'h1', 'div.ArticleBodyWrapper'],
+    ['Reuters', 'https://www.reuters.com', 'h1', 'article-body__content__17Yit'],
     ['Brookings', 'https://www.brookings.edu', 'h1', 'div.page-article'],
     ['CNN', 'https://www.cnn.com', 'h1', 'div.article__content']
 ]
+
+websites= [];
+
+for name, url, title, body in siteData:
+    websites.append(Website(name, url, title, body));
+
+crawler = Crawler();
+
+crawler.get_content(websites[0], '/library/view/web-scraping-with/9781491910283').get_data();
+
+print("\t\t", "-"*25);
+
+crawler.get_content(websites[1], 
+        '/technology/nvidias-business-practices-eu-antitrust-spotlight-sources-say-2024-12-06/')\
+    .get_data();
+
+print("\t\t", "-"*25);
+
+crawler.get_content(websites[2], 
+        '/blog/techtank/2016/03/01/idea-to-retire-old-methods-of-policy-education/').get_data();
+
+print("\t\t", "-"*25);
+
+crawler.get_content(websites[3], 
+        '/2023/04/03/investing/dogecoin-elon-musk-twitter/index.html').get_data();
