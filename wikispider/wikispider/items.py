aa60bbf9dbbb588bfd053e84a411a743cd0d70bd
Jafar Loka <gaafer.hajji1995@gmail.com>
2024-12-13 11:41:14 +0300

    Saving Data Into Items Using New Command

--- wikispider/wikispider/items.py
+++ wikispider/wikispider/items.py
@@ -10,3 +10,9 @@ class WikispiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class Article(scrapy.Item):
+    url = scrapy.Field();
+    title = scrapy.Field();
+    text = scrapy.Field();
+    last_updated = scrapy.Field();
---
+++ wikispider/wikispider/spiders/articles_02.py
@@ -0,0 +1,46 @@
+from scrapy.linkextractors import LinkExtractor;
+
+from scrapy.spiders import CrawlSpider, Rule;
+
+from scrapy.http import Response;
+
+from wikispider.items import Article;
+
+class ArticlesSpider(CrawlSpider):
+
+    name = 'articleItems';
+
+    allowed_domains = ['wikipedia.org']
+
+    start_urls = ['https://en.wikipedia.org/wiki/Benevolent_dictator_for_life'];
+
+    rules = [
+
+        Rule(
+            LinkExtractor(allow='(/wiki/)((?!:).)*$'),
+            callback='parse_items',
+            follow=True,
+        ),
+    ];
+
+    def parse_items(self, response: Response):
+
+        article = Article();
+
+        article['url']   = response.url;
+        
+        # print("\t\t", "-"*25);
+        # print(f'The URL Is: {url}');
+
+        article['title'] = response.css('span.mw-page-title-main::text').extract_first();
+        article['text']  = response.xpath('//div[@id="mw-content-text"]//text()').extract();
+        article['last_updated'] = response.css('li#footer-info-lastmod::text').extract_first();
+
+        return article;
+
+        # print(f'The Title Is: {title}');
+        # print(f'The Text Is: {text}');
+        # print(f'The Last Updated is: {last_updated}');
+        # print(f"The Title Is: {title}");
+
+        # print("\t\t", "-"*25);
